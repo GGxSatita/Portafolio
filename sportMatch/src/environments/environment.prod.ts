8e88590402847c4d7a1682936265c181e1203e83
shunnaldo <benjaminignacioparramolina@gmail.com>
2024-09-20 17:20:36 -0300

    coneccion

--- sportMatch/src/environments/environment.prod.ts
+++ sportMatch/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyBYGCbdr-FSDMtB_-PvDKf7OIJuo1-a5nE",
+    authDomain: "sportmach-fc07f.firebaseapp.com",
+    projectId: "sportmach-fc07f",
+    storageBucket: "sportmach-fc07f.appspot.com",
+    messagingSenderId: "276809292205",
+    appId: "1:276809292205:web:fcef7335a06702d4496924",
+    measurementId: "TU_MEASUREMENT_ID"
+    }
 };
